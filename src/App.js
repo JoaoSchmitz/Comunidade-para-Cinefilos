a117487adb5674678c99acf985aacb79c53fbf5b
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-03-23 18:53:55 -0300

    Basic code to obtain API data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { APIKey } from './config/APIKey';
+import { useState, useEffect } from 'react'
+import { APIKey } from './config/key';
 
 function App() {
+
+  // Como vai ser feito pra se obter os dados da API:
+
+  const [movies, setMovies] = useState([]) //recebe os filmes populares
+  const image_path = 'https://image.tmdb.org/t/p/w500'
+
+  useEffect(() => {
+    // consumir a api...
+
+    fetch(`
+    https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`)
+      .then(response => response.json())
+      .then(data => {console.log(data.results); setMovies(data.results)})
+  }, [])
+
+
   return (
     <>
-      <p>Hello World!</p>
+      <p>Comunidade para cinéfilos</p>
     </>
   );
 }
---
+++ src/config/APIKey.js
@@ -0,0 +1,1 @@
+export const APIKey = '39eb5b480c01bd3739bbed001bbb501e'
