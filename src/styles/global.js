15c3df3e5c8a73e4c82fa82482cc1d966c4c5fbd
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-03-26 10:48:03 -0300

    Adding styled-components to project

---
+++ src/styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    html {
+        --mainColor: #092B49;
+        --secondColor: #2B3844;
+        --thirdColor: #1B2630;
+        --textColor: #3FB6CA;
+        --textColor2: #CBA238;
+    }
+
+    body {
+        background-color: var(--mainColor);
+    }
+`
