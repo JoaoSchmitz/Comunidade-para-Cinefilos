c61b6c1d051b6ffe4f6100a3cf99f08ca8931b20
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-03-31 21:59:14 -0300

    Arrumando style das listas

--- src/components/PersMovieList/style.js
+++ src/components/PersMovieList/style.js
@@ -4,5 +4,7 @@ export const MovieListContainer = styled.div`
     text-align: center;
     margin: 2em 6%;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    column-gap: 2rem;
+    row-gap: 2rem;
 `
